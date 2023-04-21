41ae45170726f2c6077a00d485f15a4fee850674
Saulo17 <zthevanbix17@gmail.com>
2023-04-21 18:54:52 -0500

    Agregar archivos de proyecto.

---
+++ Loggineu/Login.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Loggineu
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = txtUsuario.Text;
+            string contraseña = txtContraseña.Text;
+            if(usuario == "usaul" && contraseña == "ss7845")
+            {
+                Navigation.PushAsync(new Inicio());
+
+            }
+            else
+            {
+                DisplayAlert("Alerta","El usuario/contraseña Incorrectos ", "Cerrar");
+                txtUsuario.Text = "";
+                txtContraseña.Text = "";
+                
+            }
+        }
+
+        private void btnRegistrar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Registro());
+        }
+    }
+}
---
+++ Loggineu/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Loggineu
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Ok_Clicked(object sender, EventArgs e)
+        {
+            //string Usuario = txtUsuario.Text;
+            //string Contraseña = txtContraseña.Text;
+
+            //if ((Usuario == "Saul") && (Contraseña == "12345"))
+            //{
+            //    Navigation.PushAsync(new Sistema());
+            //}
+            //else
+            //{
+            //    DisplayAlert("Alerta", "El usuario o contraseña son incorrectos", "OK");
+            //}
+        }
+    }
+}
